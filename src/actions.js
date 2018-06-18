90e6a8abdc5a0177380e3939a5a77445dcae4394
Gregory Wilson <greg@awnage.com>
2018-06-18 00:17:30 -0400

    cleanup from BIF

--- src/actions.js
+++ src/actions.js
@@ -2,8 +2,6 @@ import Action from 'd2-ui/lib/action/Action';
 import { getInstance as getD2 } from 'd2/lib/d2';
 import log from 'loglevel';
 
-// import appStoreStore from './stores/appStore.store';
-// import installedAppStore from './stores/installedApp.store';
 
 /**
  * Various actions that a component can subscribe to
