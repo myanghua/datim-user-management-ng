ea8951e70240933d1b403d03ad62994d27885f26
Gregory Wilson <greg@awnage.com>
2018-06-20 11:50:13 -0400

    Cache countries list to reduce network traffic. Closes #1

--- src/app/actions/core.js
+++ src/app/actions/core.js
@@ -51,22 +51,31 @@ export function setMe() {
 
 export function getCountries(d2) {
   return (dispatch, getState) => {
-    let params = {
-      paging: true,
-      fields: 'id,name,level',
-      filter: 'level:eq:3'
-    };
-    d2.models.organisationUnits.list(params).then(ous=>{
-      dispatch({ type: actions.SHOW_PROCESSING, status: true });
-      dispatch({ type: actions.SET_COUNTRIES, data: ous.toArray() });
-      dispatch({ type: actions.HIDE_PROCESSING, status: false });
 
-    })
-    .catch(e=>{
-      // @TODO:: snackbar alert
-      //d2Actions.showSnackbarMessage("Error fetching data");
-      console.error(e);
-    });
+    const countries = localStorage.getItem('countries');
+
+    if (countries) {
+      dispatch({ type: actions.SET_COUNTRIES, data: JSON.parse(countries)});
+      console.log('COUNTRIES: pulling from cache');
+    }
+    else {
+      let params = {
+        paging: false,
+        fields: 'id,name,level',
+        filter: 'level:eq:3'
+      };
+      d2.models.organisationUnits.list(params).then(ous=>{
+        dispatch({ type: actions.SHOW_PROCESSING, status: true });
+        dispatch({ type: actions.SET_COUNTRIES, data: ous.toArray() });
+        localStorage.setItem('countries', JSON.stringify(ous.toArray()));
+        dispatch({ type: actions.HIDE_PROCESSING, status: false });
+      })
+      .catch(e=>{
+        // @TODO:: snackbar alert
+        //d2Actions.showSnackbarMessage("Error fetching data");
+        console.error(e);
+      });
+    }
   }
 };
 
