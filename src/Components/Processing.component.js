9c399216035a6eb38f1147947c30ee8dd4e1e973
Gregory Wilson <greg@awnage.com>
2018-08-15 14:45:34 -0400

    Accessibility and Windows styling fix

--- src/Components/Processing.component.js
+++ src/Components/Processing.component.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 class Processing extends Component {
   render() {
     return (
-      <Dialog open={this.props.processing.processing}>
-        <CircularProgress
-          size={40}
-          color="primary"
-          thickness={7}
-          style={{ padding: "1em" }}
-        />
+      <Dialog
+        open={this.props.processing.processing}
+        aria-labelledby="alert-dialog-title"
+      >
+        <DialogTitle id="alert-dialog-title" style={{ display: "none" }}>
+          Processing...
+        </DialogTitle>
+        <DialogContent style={{ textAlign: "center", margin: "1em", paddingTop: "1em" }}>
+          <CircularProgress
+            size={40}
+            color="primary"
+            thickness={7}
+            style={{ padding: "1em" }}
+          />
+        </DialogContent>
       </Dialog>
     );
   }
